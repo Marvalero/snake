ce5bcef4c43fc6bc20e2cc68641586e7c3f7595d
Marvalero <chicemav@gmail.com>
2020-09-21 12:39:36 +0100

    Add stuff

---
+++ Snake/Assets/Script/CameraMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+     public float cameraDistOffset = 10;
+     public GameObject player;
+     private Camera mainCamera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+         mainCamera = GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+         Vector3 playerInfo = player.transform.transform.position;
+         mainCamera.transform.position = new Vector3(playerInfo.x, playerInfo.y, playerInfo.z - cameraDistOffset);
+    }
+}
+
+
---
+++ Snake/Assets/Script/PlayerControls.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControls : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public float groundCheckRadius;
+    public LayerMask whatIsGround;
+    private bool onGround;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       rb = GetComponent<Rigidbody2D>();
+       Debug.Log("Hello world!");
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       rb.velocity = new Vector2(15, rb.velocity.y);
+       onGround = Physics2D.OverlapCircle(rb.position, groundCheckRadius, whatIsGround);
+
+       if (Input.GetMouseButtonDown(0) && onGround) {
+         rb.velocity = new Vector2(rb.velocity.x, 15);
+       }
+        
+    }
+}
